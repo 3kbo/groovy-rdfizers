873f3fce5cedcc9ebcdee401b3c9bb2a5dedf6e6
Richard Hancock <richard.hancock@3kbo.com>
2011-12-18 16:44:44 +1300

    Added test java code

---
+++ src/main/java/com/_3kbo/rdfizers/App.java
@@ -0,0 +1,13 @@
+package com._3kbo.rdfizers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
